6b721558e71465239569d00e62298bc47180825a
romano6379 <romano6379@gmail.com>
2026-06-17 13:37:22 +0900

    AutoPilot: update data/site-config.js

--- data/site-config.js
+++ data/site-config.js
@@ -1,1 +1,7 @@
-window.siteConfig = { "siteName": "Wealth Builder Daily" };
+window.siteConfig = {
+  "siteName": "Wealth Builder Daily",
+  "primaryColor": "#0f766e",
+  "bgMain": "#f4fcfb",
+  "fontHeading": "'Outfit'",
+  "layoutStyle": "clean"
+};
